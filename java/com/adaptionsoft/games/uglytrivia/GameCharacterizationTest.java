519891a3386cfa91c9d7f77189fd60baeb2923e4
Ramon Salla <rsallar@gmail.com>
2014-04-13 11:18:10 +0200

    testing user wrong answer coverage 48%

--- java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
+++ java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
@@ -1,11 +1,10 @@
 package com.adaptionsoft.games.uglytrivia;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,6 +13,7 @@ public class GameCharacterizationTest {
 	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
 	
 	private final String caption1 = "Lincoln was added They are player number 1 Lincoln is the current player They have rolled a 1 Lincoln's new location is 1 The category is Science Science Question 0 ";
+	private final String caption2 = "Lincoln was added They are player number 1 Question was incorrectly answered Lincoln was sent to the penalty box ";
 		
 	
 	@Before
@@ -28,5 +28,13 @@ public class GameCharacterizationTest {
 		game.roll(1);
 		assertEquals(caption1, out.toString().replace("\r\n", " "));
 	}
+	
+	@Test
+	public void run_with_single_user_when_goes_to_penalty_box(){
+		game.add("Lincoln");
+		game.wrongAnswer();
+		assertEquals(caption2, out.toString().replace("\r\n", " "));
+	}
+	
 
 }
