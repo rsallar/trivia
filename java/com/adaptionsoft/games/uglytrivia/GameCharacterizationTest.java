2c68fd16277314e7ed773562b38bf7aa996bc3aa
Ramon Salla <rsallar@gmail.com>
2014-04-13 11:35:40 +0200

    * testing user right answer when getting out of penalty box * coverage 79%

--- java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
+++ java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
@@ -17,6 +17,7 @@ public class GameCharacterizationTest {
 	private final String caption3 = "Lincoln was added They are player number 1 Question was incorrectly answered Lincoln was sent to the penalty box Lincoln is the current player They have rolled a 2 Lincoln is not getting out of the penalty box ";
 	private final String caption4 = "Lincoln was added They are player number 1 Question was incorrectly answered Lincoln was sent to the penalty box Lincoln is the current player They have rolled a 1 Lincoln is getting out of the penalty box Lincoln's new location is 1 The category is Science Science Question 0 "; 
 	private final String caption5 = "Lincoln was added They are player number 1 Answer was corrent!!!! Lincoln now has 1 Gold Coins. Lincoln is the current player They have rolled a 1 Lincoln's new location is 1 The category is Science Science Question 0 ";
+	private final String caption6 = "Lincoln was added They are player number 1 Question was incorrectly answered Lincoln was sent to the penalty box Lincoln is the current player They have rolled a 1 Lincoln is getting out of the penalty box Lincoln's new location is 1 The category is Science Science Question 0 Answer was correct!!!! Lincoln now has 1 Gold Coins. ";
 	
 	@Before
 	public void before(){
@@ -60,6 +61,15 @@ public class GameCharacterizationTest {
 		game.wasCorrectlyAnswered();
 		game.roll(1);
 		assertEquals(caption5, out.toString().replace("\r\n", " "));
+	}
+	
+	@Test
+	public void user_right_answer_when_getting_out_of_penalty_box(){
+		game.add("Lincoln");
+		game.wrongAnswer();
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		assertEquals(caption6, out.toString().replace("\r\n", " "));
 	}
 	
 
