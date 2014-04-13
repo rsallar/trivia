de52796516bfa25320f2ae3e026807780039d375
Ramon Salla <rsallar@gmail.com>
2014-04-13 12:03:08 +0200

    refactoring new funcionality sixth player to new GameTest class

---
+++ java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -0,0 +1,26 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+	private Game game;
+	@Before
+	public void before(){
+	
+		game = new Game();
+	}
+
+	@Test
+	public void game_should_allow_a_sixth_user(){
+		game.add("Lincoln");
+		game.add("Zapatero");
+		game.add("Hollande");
+		game.add("Evita");
+		game.add("Karl");
+		
+		assertEquals(true, game.add("Einstein"));
+	}
+}
