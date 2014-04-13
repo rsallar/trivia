65f61f7fc83bc5010776a595e9b1bef4828d3497
Ramon Salla <rsallar@gmail.com>
2014-04-13 11:29:41 +0200

    * testing right answer when not in penalty box * coverage 71%

--- java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
+++ java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
@@ -16,6 +16,7 @@ public class GameCharacterizationTest {
 	private final String caption2 = "Lincoln was added They are player number 1 Question was incorrectly answered Lincoln was sent to the penalty box ";
 	private final String caption3 = "Lincoln was added They are player number 1 Question was incorrectly answered Lincoln was sent to the penalty box Lincoln is the current player They have rolled a 2 Lincoln is not getting out of the penalty box ";
 	private final String caption4 = "Lincoln was added They are player number 1 Question was incorrectly answered Lincoln was sent to the penalty box Lincoln is the current player They have rolled a 1 Lincoln is getting out of the penalty box Lincoln's new location is 1 The category is Science Science Question 0 "; 
+	private final String caption5 = "Lincoln was added They are player number 1 Answer was corrent!!!! Lincoln now has 1 Gold Coins. Lincoln is the current player They have rolled a 1 Lincoln's new location is 1 The category is Science Science Question 0 ";
 	
 	@Before
 	public void before(){
@@ -51,6 +52,14 @@ public class GameCharacterizationTest {
 		game.wrongAnswer();
 		game.roll(1);
 		assertEquals(caption4, out.toString().replace("\r\n", " "));
+	}
+	
+	@Test
+	public void user_right_answer_when_not_in_penalty_box(){
+		game.add("Lincoln");
+		game.wasCorrectlyAnswered();
+		game.roll(1);
+		assertEquals(caption5, out.toString().replace("\r\n", " "));
 	}
 	
 
